ca57b0bfa44440ffb75977924e29a02db02e043a
Guilherme Assunção <cgmatosassuncao@gmail.com>
2020-10-13 23:48:31 -0300

    Olhando as oportunidades

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,5 +1,13 @@
+const option = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
 //Create Map
-const mymap = L.map("mapid").setView([-18.7171333,-39.8566417], 15)
+const mymap = L.map("mapid", option).setView([-18.7171333, -39.8566417], 15)
 
 //Create TileLayer
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(mymap)
@@ -7,20 +15,34 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(mymap)
 //Create Icon
 const icon = L.icon({
     iconUrl: "./public/images/map-marker.svg",
-    iconSize: [58,68],
-    iconAnchor: [29,68],
-    popupAnchor:[170,2]
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
 })
 
-//Create Popup
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" /> </a>')
-
 //Create Add Marker
-L.marker([-18.7171333,-39.8566417], {icon})
-  .addTo(mymap)
-  .bindPopup(popup)
+L.marker([-18.7171333, -39.8566417], {
+        icon
+    })
+    .addTo(mymap)
+
+
+//Selecionar Imagem
+function selectImage(event) {
+    const button = event.currentTarget;
+
+    const buttons = document.querySelectorAll(".images button");
+
+    buttons.forEach(removeActive)
+
+    function removeActive(button) {
+        button.classList.remove("active");
+    }
+
+    button.classList.add("active");
+
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    imageContainer.src = image.src
+}
