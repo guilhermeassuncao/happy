2bafcb1bc4ce1f0c70923e0e140d45d3a9983fbf
Guilherme Assunção <cgmatosassuncao@gmail.com>
2020-10-15 13:04:04 -0300

    Metade 3 Dia

---
+++ src/server.js
@@ -0,0 +1,53 @@
+//Importar Dependencias
+const express = require('express')
+const path = require('path')
+const pages = require('./pages.js')
+
+//Iniciar o express
+const server = express()
+
+
+server
+//Iniciando Arquivos Estaticos
+.use(express.static('public'))
+//Configurar Template Engine
+.set('views', path.join(__dirname, 'views'))
+.set('view engine','hbs')
+//Criar uma Rota
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage)
+//Ligar Servidor
+.listen(5500)
+
+
+
+// const {
+//     pageLanding,
+//     pageStudy,
+//     pageGiveClasses,
+//     saveClasses
+// } = require('./pages')
+
+// //Configura Nunjucks
+// const nunjucks = require('nunjucks')
+// nunjucks.configure('src/views', {
+//     express: server,
+//     noCache: true,
+// })
+
+// //Inicio da Configuração do Servidor
+// server
+//     //Receber os dados do req.body
+//     .use(express.urlencoded({
+//         extended: true
+//     }))
+//     //Configura Arquivos Estáticos (Css, Scripts e Imagens)
+//     .use(express.static('public'))
+//     //Rotas da Aplicação
+//     .get('/', pageLanding)
+//     .get('/study', pageStudy)
+//     .get('/give-classes', pageGiveClasses)
+//     .post('/save-classes', saveClasses)
+//     .listen(5500);
